6f16de0b2ed8328d0da2777033c7886fb94a0f47
Adam Murray <adam.murray1@mycit.ie>
2022-12-02 12:13:19 +0000

    hello 7 working

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ def main():
             id, time_taken, venue = race_results(races_location)
             fastest_runner = winner_of_race(id, time_taken)
             display_races(id, time_taken, venue, fastest_runner)
-        elif input_menu != 2:
+        elif input_menu == 2:
             users_venue(races_location, runners_id)
         elif input_menu == 3:
             competitors_by_county(runners_name, runners_id)
@@ -245,6 +245,8 @@ def main():
             displaying_race_times_one_competitor(races_location, runner, id)
         elif input_menu == 6:
             displaying_runners_who_have_won_at_least_one_race(races_location, runners_name, runners_id)
+        elif input_menu == 7:
+            exit()
         print()
         input_menu = read_integer_between_numbers(MENU, 1, 7)
     updating_races_file(races_location)
